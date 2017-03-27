ac9dd6a97b75f5e5b11e882cad948f2a2ae4caf3
MagistrZP <magistr-zp@mail.ru>
2017-03-27 22:25:46 +0300

    update bd

--- hakaton.sql
+++ hakaton.sql
@@ -2,10 +2,10 @@
 -- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
--- Хост: 127.0.0.1:3306
--- Время создания: Мар 27 2017 г., 16:11
--- Версия сервера: 5.5.53
--- Версия PHP: 7.1.0
+-- Host: 127.0.0.1:3306
+-- Generation Time: Mar 27, 2017 at 10:24 PM
+-- Server version: 5.5.53
+-- PHP Version: 7.1.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,13 +17,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `hakaton`
+-- Database: `hakaton`
 --
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `task`
+-- Table structure for table `task`
 --
 
 CREATE TABLE `task` (
@@ -32,13 +32,22 @@ CREATE TABLE `task` (
   `description` text NOT NULL,
   `user_id` int(11) UNSIGNED NOT NULL,
   `deadline` datetime NOT NULL,
-  `complete` int(11) NOT NULL DEFAULT '0'
+  `complete` enum('0','1') NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `task`
+--
+
+INSERT INTO `task` (`id_task`, `task_name`, `description`, `user_id`, `deadline`, `complete`) VALUES
+(1, 'first task', 'test 1', 1, '2017-03-30 00:00:00', '1'),
+(2, 'second task', 'test 2', 2, '2017-03-27 22:16:57', '0'),
+(3, 'third task', 'test 3', 3, '2017-03-27 22:17:21', '0');
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `user`
+-- Table structure for table `user`
 --
 
 CREATE TABLE `user` (
@@ -49,44 +58,46 @@ CREATE TABLE `user` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Дамп данных таблицы `user`
+-- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `name`, `password`, `role`) VALUES
 (1, 'Ксения', 'e10adc3949ba59abbe56e057f20f883e', '1'),
 (2, 'Дима', 'e10adc3949ba59abbe56e057f20f883e', '1'),
-(3, 'user1', 'e10adc3949ba59abbe56e057f20f883e', '0');
+(3, 'user1', 'e10adc3949ba59abbe56e057f20f883e', '0'),
+(4, 'new_user', '0eb56ababcc403f0fe3e6f09fc1d7708', '1'),
+(5, 'worker1', '0eb56ababcc403f0fe3e6f09fc1d7708', '2');
 
 --
--- Индексы сохранённых таблиц
+-- Indexes for dumped tables
 --
 
 --
--- Индексы таблицы `task`
+-- Indexes for table `task`
 --
 ALTER TABLE `task`
   ADD PRIMARY KEY (`id_task`);
 
 --
--- Индексы таблицы `user`
+-- Indexes for table `user`
 --
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT для сохранённых таблиц
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT для таблицы `task`
+-- AUTO_INCREMENT for table `task`
 --
 ALTER TABLE `task`
-  MODIFY `id_task` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id_task` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
--- AUTO_INCREMENT для таблицы `user`
+-- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
