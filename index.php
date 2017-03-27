9ff0f9f366d0ca2ec43b598d20d59b51e55f9656
seleznevaksenia <380992717715@ya.ru>
2017-03-28 02:57:04 +0300

    1

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 session_start();
-
+//ррррр
 // Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 define('img_dir', ROOT.'/template/images/captcha/background/');
